ad1fcc768cf27c059f201288be080b87e8a34381
tjacek <tjacek@agh.edu.pl>
2022-09-11 10:26:57 +0200

    simple example

---
+++ heat.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+def pend(y, t, b, c):
+    theta, omega = y
+    dydt = [omega, -b*omega - c*np.sin(theta)]
+    return dydt
+
+y0 = [np.pi - 0.1, 0.0]
+t = np.linspace(0, 10, 101)
+b,c=0.25,5
+sol = odeint(pend, y0, t, args=(b, c))
+plt.plot(t, sol[:, 0], 'b', label='theta(t)')
+plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+plt.legend(loc='best')
+plt.xlabel('t')
+plt.grid()
+plt.show()
