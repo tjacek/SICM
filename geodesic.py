52f0e7e98fee93e144e5e0d2062916410c1652f7
tjacek <tjacek@agh.edu.pl>
2024-08-13 02:30:07 +0200

    chris coeff

--- geodesic.py
+++ geodesic.py
@@ -1,6 +1,19 @@
 from scipy.integrate import odeint
 import numpy as np
 
+class ChristoffelSymbols(object):
+    def x_det_x(self,x,y):
+        return 0,np.sin(y)*np.cos(y)
+
+    def x_det_x(self,x,y):
+        return (-np.tan(y)),0
+
+    def x_det_y(self,x,y):
+        return (-np.tan(y)),0
+
+    def y_det_y(self,x,y):
+        return 0,0
+
 def returns_dydt(y,t):
     dydt = -y * t + 13
     return dydt
