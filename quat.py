fea7c496030763dba7ea0cdd94726cb52a698a35
tjacek <jacekt89@gmail.com>
2026-04-11 18:42:12 +0200

    addition

--- quat.py
+++ quat.py
@@ -28,11 +28,12 @@ class Quaternion(object):
         new_arr=np.dot(A,B)
         new_vec=self.from_matrix(new_arr)
         return Quaternion(*new_vec)
-#        a= self.a*q.a - self.b*q.b - self.c*q.c - self.d*q.d
-#        b= self.a*q.b + self.b*q.a + self.c*q.d - self.d*q.c
-#        c= self.a*q.c - self.b*q.d + self.c*q.a + self.d*q.b
-#        d= self.a*q.d + self.b*q.c - self.c*q.b + self.d*q.a
-#        return Quaternion(a,b,c,d)
+
+    def __add__(self,q):
+        return Quaternion(self.a+q.a, 
+                          self.b+q.b, 
+                          self.c+q.c, 
+                          self.d+q.d )
     
     def __repr__(self):
         return f"{self.a},{self.b},{self.c},{self.d}"
@@ -43,3 +44,4 @@ k=Quaternion(d=1)
 print(i*i)
 print(j*j)
 print(k*k)
+print(i+j)
